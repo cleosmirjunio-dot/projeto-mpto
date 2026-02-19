4a8f5bbfcf8dfd1a69aaa7afe51ea877149b1d0b
cleosmirjunio-dot <cleosmirjunio@gmail.com>
2026-02-19 17:47:42 -0300

    Alteração da docst

--- src/github_client.py
+++ src/github_client.py
@@ -1,6 +1,21 @@
 import requests
+from src.reposito
 class GithubClient:
     BASE_URL = "https://api.github.com/users"
 
-    def get_respos(self, username: str) -> list[dict]:
+    def get_repos(self, username: str) -> list[dict]:
+        '''
+        Busca os repositórios públicos de um usuário no GitHub
+        Args:
+            username (str) : Nome do usuário no GitHub
+
+        Returns:
+            lis[dict]: lista de repositórios em formato JSON
+
+        raises:
+            valueError: Se o usuário não existir ou limite for atingido.
+            connectionError: Se houver falha de conexão
+            TimeoutError: Se a requisição demorar demais.
+        '''
+
         pass
